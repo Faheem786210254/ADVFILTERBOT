38ff3a6fd613edc481b3a4195aa47779dbbab3c9
Arshad3721 <124030789+Arshad3721@users.noreply.github.com>
2023-09-12 20:24:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ MELCOW_VID = environ.get("MELCOW_VID")
 SPELL_IMG = environ.get("SPELL_IMG")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5921027710').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', ' ').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hackpok:hackpok@cluster0.vxzb3f3.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Kingkong:Kingkong@cluster0.qhimj7h.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
@@ -56,13 +56,13 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "8")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesrequestfiles')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Moviesrequest_info')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_open_Mdisk_pro_links/2')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+iQmKO11V39wwZWY1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+RsH8kf1YHZlkNWE1')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/backupchannaljoin')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello Everyone ❤️')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001804171666'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Moviesrequest_chat')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
